665ff00f31f48742b6f10d7b8cc0b81b4f2bd184
briefs33 <capsule.briefs@gmail.com>
2022-06-05 08:39:02 +0200

    vytvorenie okna pre pridanie zapisu

--- Diar/diar.py
+++ Diar/diar.py
@@ -5,8 +5,15 @@ import tkinter.ttk as ttk
 
 
 def pridat():
-    textBox.insert(0, zaznamInput.get() + '\n')
+    window_pridat = tk.Toplevel()
+    zaznamLabel = ttk.Label(window_pridat, text = "Záznam:")
+    zaznamInput = ttk.Entry(window_pridat)
 
+    zaznamLabel.grid(row = 0, column = 0)
+    zaznamInput.grid(row = 0, column = 1)
+    textBox.insert(0, zaznamInput.get() + '\n')
+    zatvoritButton = ttk.Button(window_pridat, text = "Zapísať", command = window_pridat.destroy)
+    zatvoritButton.grid(row = 5, column = 1)
 
 
 
@@ -14,16 +21,10 @@ root = tk.Tk()
 root.title('Diár')
 root.grid()
 
-zaznamLabel = ttk.Label(root, text = "Záznam:")
-zaznamInput = ttk.Entry(root)
-
-zaznamLabel.grid(row = 0, column = 0)
-zaznamInput.grid(row = 0, column = 1)
-
 textBox = tk.Listbox(root, width = 50)
 textBox.grid(row = 4, columnspan = 4)
 
-pridatButton = ttk.Button(root, text = "Pridať", command = pridat)
+pridatButton = ttk.Button(root, text = "Pridať", command = lambda: pridat())
 pridatButton.grid(row = 5, column = 1)
 
 
